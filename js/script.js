c9653da1f32c7da5a085dfb132e1258030a03551
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2020-11-13 11:03:27 -0300

    Melhorias no design

--- js/script.js
+++ js/script.js
@@ -7,21 +7,21 @@ let redColor = 0
 let greenColor = 0
 let blueColor = 0
 
-$R.addEventListener('change', ()=>{
+$R.addEventListener('input', ()=>{
     redColor = $R.value
     $corAtual.style.backgroundColor = `rgb(${redColor}, ${greenColor}, ${blueColor})`
     document.getElementById('seletores-red').querySelector('p').innerText = `Red: ${redColor}`
     $corAtual.querySelector('div').innerText = `#${(Number(redColor)).toString(16).toUpperCase()}${(Number(greenColor)).toString(16).toUpperCase()}${(Number(blueColor)).toString(16).toUpperCase()}`
 })
 
-$G.addEventListener('change', ()=>{
+$G.addEventListener('input', ()=>{
     greenColor = $G.value
     $corAtual.style.backgroundColor = `rgb(${redColor}, ${greenColor}, ${blueColor})`
     document.getElementById('seletores-green').querySelector('p').innerText = `Green: ${greenColor}`
     $corAtual.querySelector('div').innerText = `#${(Number(redColor)).toString(16).toUpperCase()}${(Number(greenColor)).toString(16).toUpperCase()}${(Number(blueColor)).toString(16).toUpperCase()}`
 })
 
-$B.addEventListener('change', ()=>{
+$B.addEventListener('input', ()=>{
     blueColor = $B.value
     $corAtual.style.backgroundColor = `rgb(${redColor}, ${greenColor}, ${blueColor})`
     document.getElementById('seletores-blue').querySelector('p').innerText = `Blue: ${blueColor}`
